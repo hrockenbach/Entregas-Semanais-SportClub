244e470a8d91e7d53c4f4edfd66fbb5ac708a5c5
Helen Rockenbach <helen.rockenbach14@gmail.com>
2024-09-30 11:37:28 -0300

    terceiro commit

--- back/src/controller/perfilController.js
+++ back/src/controller/perfilController.js
@@ -3,8 +3,8 @@ const dotenv = require('dotenv').config();
 
 // Função para buscar informações de perfil do banco de dados
 const getPerfil = (req, res) => {
-  const userId = req.params.id_usuario; // Pega o ID da URL corretamente
-
+  const userId = req.params.id; // Pega o ID da URL corretamente
+  console.log(userId)
   const sql = 'SELECT * FROM usuarios WHERE id = ?';
 
   connection.query(sql, [userId], (err, result) => {
@@ -16,12 +16,12 @@ const getPerfil = (req, res) => {
     // } else {
     //   res.status(404).send('Usuário não encontrado');
     // }
-
+    console.log(result)
     if (result) {
       res.status(201).json({
         success: true,
         message: "Sucesso com a busca do usuário!!",
-        data: results,
+        data: result,
       });
     } else {
       res.status(400).json({
